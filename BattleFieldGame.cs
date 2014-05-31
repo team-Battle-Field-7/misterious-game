0cb88e50a6a707ebe9dc4654dc41d9aef72406bb
vnci <vbahovski@gmail.com>
2014-05-31 09:38:15 +0300

    Fixed BattleFieldGame.cs's summary

--- BattleFieldGame.cs
+++ BattleFieldGame.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace BattleFieldNamespace
 {
     /// <summary>
-    /// Ако това ще е енджин на играта, няма да я наследява, а ще извиква <c>Init()</c> и <c>Start()</c> от нея.
+    /// Ако това ще е енджин на играта, няма да я наследява, а ще извиква публичните методи от нея.
     /// Методите, които са private в класа Battlegame са само такива, използвани помощно от дрги методи //vnci    /// 
     /// </summary>
     class BattleField : BattleGame
