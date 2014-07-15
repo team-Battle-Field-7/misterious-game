189bddff52388ee82cb2bdafb8282576b829bd7c
vnci <vbahovski@gmail.com>
2014-07-15 17:23:46 +0300

    Same Unit tests (added to repository)

---
+++ BattleField7UnitTests/LoggerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleField7Namespace;
+
+namespace BattleField7UnitTests
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void TestWrongFileLog()
+        {
+            var logger = new Logger();
+            logger.FilePath = "log.html";
+        }
+
+        [TestMethod]
+        public void TestLogFileExists()
+        {
+            var logger = new Logger();
+            logger.Log("testing!");
+            Assert.IsTrue(System.IO.File.Exists(logger.FilePath), "No log file created!");
+            Assert.IsTrue(System.IO.File.ReadAllText(logger.FilePath).Length > 0, "Log file was created (" + logger.FilePath + ") but nothing was written inside.");
+        }
+    }
+}
